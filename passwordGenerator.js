1ee1f18519838f34adb91c00dbb09d7b36acee88
Eric Hannafin <erichannafin1990@gmail.com>
2025-02-25 21:59:02 +0000

    Changing password text area to div to fix issue where password was not populating

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,19 +1,6 @@
 const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
     "/"];
-
 let passwordLength = 12;
-
-function generateRandomPassword() {
-    let password = "";
-    for (let i = 0; i <= passwordLength; i++) {
-        password += characters[Math.floor(Math.random() * characters.length)]
-    }
-    alert(password)
-}
-
-console.log(generateRandomPassword());
-
-
 
 let textArea1 = document.getElementById("password-text-area1");
 let textArea2 = document.getElementById("password-text-area2");
@@ -22,3 +9,17 @@ let textArea4 = document.getElementById("password-text-area4");
 
 document.getElementById("generate-password-button1").addEventListener("click", generateRandomPassword);
 document.getElementById("generate-password-button2").addEventListener("click", generateRandomPassword);
+
+function generateRandomPassword() {
+    let password1 = "";
+    let password2 = "";
+    for (let i = 0; i < passwordLength; i++) {
+        password1 += characters[Math.floor(Math.random() * characters.length)];
+        password2 += characters[Math.floor(Math.random() * characters.length)];
+    }
+    textArea1.textContent = password1;
+    textArea2.textContent = password2;
+}
+
+
+
